1c203dc74fac8e51015e43d913d31d67910ab024
Everus <everuslainus@gmail.com>
2023-12-23 18:01:47 +0530

    parse and get peers from tracker using libtorrent

--- try_oxxopts_lib.cpp
+++ try_oxxopts_lib.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "/opt/homebrew/include/libtorrent/session.hpp"
 #include "/opt/homebrew/include/libtorrent/torrent_info.hpp"
 #include "/opt/homebrew/include/libtorrent/announce_entry.hpp"
 #include "cxxopts.hpp"
@@ -28,7 +29,6 @@ int main(int argc, char* argv[]) {
         std::cout<<"destination path: " <<destination_path << std::endl;
     }
 
-        try {
         libtorrent::torrent_info ti(source_path );
         std::cout << "Torrent name: " << ti.name() << std::endl;
         std::cout << "Piece length: " << ti.piece_length() << " bytes" << std::endl;
@@ -39,10 +39,38 @@ int main(int argc, char* argv[]) {
             std::cout << "  " << tracker.url << std::endl;
         }
 
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
+    //} catch (const std::exception& e) {
+    //    std::cerr << "Error: " << e.what() << std::endl;
+    //    return 1;
+    //}
 
+//get peers from the tracker
+
+    libtorrent::session ses;
+    
+    libtorrent::add_torrent_params params;
+    params.ti = std::make_shared <libtorrent::torrent_info>(ti);
+    libtorrent::torrent_handle th = ses.add_torrent(params);
+
+    //start the session
+    ses.start_upnp();
+
+    while(true){
+        ses.post_torrent_updates();
+
+        libtorrent::torrent_status status = th.status();
+
+            // Print information about the number of peers from the tracker
+            std::cout << "Number of peers from tracker: " << status.list_peers << std::endl;
+
+        std::vector<libtorrent::peer_info> peers;
+        th.get_peer_info(peers);
+
+        for(const auto&peer : peers){
+            std::cout << "Peer IP : "<<peer.ip.address().to_string() <<", Port: "<< peer.ip.port() << std::endl;     
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
     return 0;
 }

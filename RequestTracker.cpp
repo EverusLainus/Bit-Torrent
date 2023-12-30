8841e62ea0b7a4a9418fde2a313fdb279d68d158
Everus <everuslainus@gmail.com>
2023-12-30 17:35:44 +0530

    requesting tracker for peers

--- RequestTracker.cpp
+++ RequestTracker.cpp
@@ -4,6 +4,10 @@
 #include <iomanip> //for setw()
 #include <iostream>
 #include <random>
+#include "ParseTorrentFile.h"
+#include "RequestTracker.hpp"
+#include "cpr/cpr.h"
+#include "./cpp-bencoding/include/bencoding.h"
 
 //get announce URL
 std::string get_PeerID(){ 
@@ -46,9 +50,42 @@ std::string get_PeerID(){
     return encoded.str();
 }
 
-tracker_request(){
-    std::string announceUrl = TorrentFileParser::getAnnounce();
-    std::string info_hash = torrentFileParser::getInfoHash();
+//assumes someone has filled the tracker Request class
+std::vector <peers *> get_peers(){
+    TrackerRequest trackerRequest;
+    std::string anounceurl = trackerRequest.get_announce();
+    std::string infohash = trackerRequest.infohash;
+    std::string peerid = get_PeerID();
+    int port = trackerRequest.port;
+
+    //construct the tracker request URL
+    std::string trackerUrl = anounceurl + "?info_hash="+ infohash+
+    "&peer_id="+peerid 
+    +"&port="+std::to_string(port)
+    +"&uploaded="+std::to_string(trackerRequest.uploaded)
+    +"&downloaded=" +std::to_string(trackerRequest.downloaded)
+    +"&left=" + std::to_string(trackerRequest.left)
+    +"&event=" + std::to_string(trackerRequest.event);
+
+    //send the tracker request
+    auto response = cpr::Get(cpr::Url{trackerUrl});
     
+
+    //check if the request was successful
+    if(response.status_code == 200){
+        std::cout <<"Tracker response: "<<response.text <<std::endl;
+        std::shared_ptr<bencoding::BItem> decodedResponse = bencoding::Decode(response);
+            if (auto dictionary = std::dynamic_pointer_cast<bencoding::BDictionary>(decodedResponse)) {
+            // Access and print the parsed information
+            for (const auto& entry : dictionary->getMap()) {
+                std::cout << entry.first << ": " << entry.second->toString() << std::endl;
+            }
+    }else{
+        std::cerr <<"Tracker request failed with status code: "<<response.status_code<<std::endl;
+    }
+}
+
+void TrackerResponse(){
+
 }
 
